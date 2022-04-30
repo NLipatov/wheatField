939c1063d5836df53b3132c3b0224de5d47c2f1d
LipatovNN <lipatovnn@yahoo.com>
2022-04-30 11:53:58 +0300

    minor improvements

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,9 +3,19 @@ import dateService from '../../services/dateService';
 import HeavenlyBody from '../HeavenlyBody/HeavenlyBody';
 import Field from '../field/Field';
 import Clouds from '../clouds/Clouds';
+import React, {useEffect, useState} from 'react';
 
 function App() {
   const {currentTime, secondsSinceMidnightTime, dayPercentage, heightPercentage} = dateService();
+  const [getTime, setTime] = useState(0);
+  const updater = () => {
+    setTimeout(() =>{
+      setTime(getTime + 1);
+    }, 10000);
+  };
+
+  updater();
+
   return (
     <div className="App">
       <HeavenlyBody 
